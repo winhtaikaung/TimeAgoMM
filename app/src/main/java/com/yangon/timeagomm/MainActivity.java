67a2acb52698521aa56addac6b61b9f7e4aaf354
တင်းထိုက်အောင် <winhtaikaung28@hotmail.com>
2017-07-28 08:19:15 +0800

    Lib project added

--- app/src/main/java/com/yangon/timeagomm/MainActivity.java
+++ app/src/main/java/com/yangon/timeagomm/MainActivity.java
@@ -1,7 +1,13 @@
 package com.yangon.timeagomm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.yangon.library.TimeAgoFormat;
+import com.yangon.library.TimeAgoMM;
+
+import java.text.ParseException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            String time = TimeAgoMM.format("31-08-2013 10:20:56", TimeAgoFormat.DDMYYYY_HHMMSS);
+            Log.e("TIME_AGO", TimeAgoMM.format("31-08-2013 10:20:56", TimeAgoFormat.DDMYYYY_HHMMSS));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
